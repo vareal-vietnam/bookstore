94baef0e38009611496bf82cbef9fba118c67908
Canh Dinh <nguyen.canh.dinh@vareal.vn>
2019-07-11 17:23:37 +0700

    Implement book detail layout

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,9 @@ class BooksController < ApplicationController
   def index
     @books = Book.all.page(params[:page]).per(10)
   end
+
+  def show
+    @book = Book.find_by(id: book_path.match(/\d+\z/).to_s.to_i)
+    @book_image = Image.where(book_id: @book.id)
+  end
 end
