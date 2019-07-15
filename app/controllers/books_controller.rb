0ada8a440b6b4207b86b3b08474e78180e62eefe
canhdinhvareal <51991921+canhdinhvareal@users.noreply.github.com>
2019-07-15 11:17:32 +0700

    Book detail (#9)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,13 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all.page(params[:page]).per(10)
+    @books = Book.all.includes(:images).page(params[:page]).per(10)
+  end
+
+  def show
+    @book = Book.find_by(id: params[:id])
+    return if @book.present?
+
+    flash[:danger] = t('not_found')
+    redirect_to books_url
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-99.times do
-  Book.create!(
+40.times do |f|
+  book = Book.create!(
     name: Faker::Name.name,
     price: Random.rand(1..99),
     quantity: Random.rand(1..99)
   )
+  3.times do
+    id = rand(1..100)
+    Image.create!(
+      url: "https://picsum.photos/id/#{id}/600/900",
+      book_id: book.id
+    )
+  end
 end
