e371929896e096eb99062dd3ea0b7c21eb9c0de8
Canh Dinh <nguyen.canh.dinh@vareal.vn>
2019-07-15 09:47:39 +0700

    Edit translation for footer and edit logic code of Book list

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all.page(params[:page]).per(10)
+    @books = Book.all.includes(:images).page(params[:page]).per(10)
   end
 
   def show
