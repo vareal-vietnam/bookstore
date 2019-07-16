c1b38dc28e5a89696ac8798f04d0d968abc049d2
Hieu Nguyen <10509903+hieunguyentrung@users.noreply.github.com>
2019-07-16 09:22:18 +0700

    Add shoulda matchers gem (#12)

---
+++ spec/models/book_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:price) }
+  it { should validate_presence_of(:quantity) }
+
+  it { should have_many(:images).dependent(:destroy) }
+end
