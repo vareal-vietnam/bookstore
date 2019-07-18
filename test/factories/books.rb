4d7eb045d5f8631281545cfff01bcd7255a797a2
Canh Dinh <nguyen.canh.dinh@vareal.vn>
2019-07-18 13:30:22 +0700

    Fix red code third time

--- test/factories/books.rb
+++ test/factories/books.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     name { Faker::Book.title }
     price { rand(10..100) }
     quantity { rand(1..100) }
-    description { Faker::Lorem.sentence }
+    description { 'Faker::Lorem.sentence' }
 
     user
   end
