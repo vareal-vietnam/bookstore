aaa9faf3f2134e556319743efd818828c54ee3eb
Canh Dinh <nguyen.canh.dinh@vareal.vn>
2019-07-19 15:47:07 +0700

    Fix unit test, modify footer auto float bottom and Creat templete for update user's info

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
   has_many :images, dependent: :destroy
   belongs_to :user
-  delegate :name, :avatar, :address, :phone, to: :user, prefix: 'seller'
+  delegate :name, :avatar, :address, :phone, to: :user, prefix: :seller
   validates :name, presence: true
   validates :price, presence: true
   validates :quantity, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,49 @@
+default_password = "123456".freeze
+
+def generate_image_url
+  id = rand(1..900)
+  "https://picsum.photos/id/#{id}/600/900"
+end
+
+def generate_avatar_url
+  id = rand(1..900)
+  "https://picsum.photos/id/#{id}/600/600"
+end
+
+def generate_phone_number
+  "0" + rand(100000000..899999999).to_s
+end
+
 User.create!(
     name: 'Admin',
-    password: "123456",
-    password_confirmation: "123456",
+    password: "default_password",
+    password_confirmation: "default_password",
     address: Faker::Address.city,
-    phone: '0963482125',
-    avatar: "https://i.pravatar.cc/1000"
+    phone: '0123456789',
+    avatar: generate_avatar_url
   )
 
 10.times do
   user = User.create!(
     name: Faker::Name.first_name,
-    password: "123456",
-    password_confirmation: "123456",
+    password: "default_password",
+    password_confirmation: "default_password",
     address: Faker::Address.city,
-    phone: "0" + Random.rand(100000000..899999999).to_s,
-    avatar: "https://i.pravatar.cc/1000"
+    phone: generate_phone_number,
+    avatar: generate_avatar_url
   )
   5.times do
     book = Book.create!(
       name: Faker::Book.title,
-      price: Random.rand(10..999),
-      quantity: Random.rand(1..20),
+      price: rand(10..999),
+      quantity: rand(1..20),
       description: Faker::Lorem.sentence,
       user_id: user.id
     )
     3.times do
-      id = rand(1..900)
+
       Image.create!(
-        url: "https://picsum.photos/id/#{id}/600/900",
+        url: generate_image_url,
         book_id: book.id
       )
     end
