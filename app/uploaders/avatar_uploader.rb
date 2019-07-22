f3d1433b072db99766e134eb4c2489a52379ad62
mrcuong84 <nguyen.manh.cuong@vareal.vn>
2019-07-22 11:29:22 +0700

    remove () in defaul url method

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -6,7 +6,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  def default_url()
+  def default_url
     ActionController::Base.helpers.image_path('BookLogo.jpg')
   end
 end
