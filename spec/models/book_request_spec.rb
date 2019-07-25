c6f037cdf8e54d816367b12823ac70c248849950
banguyen <nguyen.huu.ba@vareal.vn>
2019-07-25 16:33:04 +0700

    resolve-mrcuong-comment

--- spec/models/book_request_spec.rb
+++ spec/models/book_request_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe BookRequest, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:quantity) }
   it { should validate_presence_of(:budget) }
+  it { should belong_to(:user) }
   it { should validate_numericality_of(:quantity).only_integer }
   it do
     should validate_numericality_of(:quantity).is_greater_than_or_equal_to(0)
