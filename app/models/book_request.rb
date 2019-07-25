54476fa528469ce4e879d13e7c90b8f9f6eb3143
banguyen <nguyen.huu.ba@vareal.vn>
2019-07-25 15:02:05 +0700

    template-test-model

--- app/models/book_request.rb
+++ app/models/book_request.rb
@@ -2,7 +2,9 @@ class BookRequest < ApplicationRecord
   belongs_to :user
   has_many :book_request_image, dependent: :destroy
   validates :name, presence: true
-  validates :quantity, presence: true
-  validates :budget, presence: true
-
+  validates :quantity, presence: true,
+                       numericality:
+                       { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :budget, presence: true,
+                     numericality: { only_integer: true, greater_than: 0 }
 end
--- spec/models/book_request_spec.rb
+++ spec/models/book_request_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe BookRequest, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:quantity) }
   it { should validate_presence_of(:budget) }
+  it { should validate_numericality_of(:quantity).only_integer }
+  it do
+    should validate_numericality_of(:quantity).is_greater_than_or_equal_to(0)
+  end
+  it { should validate_numericality_of(:budget).is_greater_than(0) }
+  it { should validate_numericality_of(:budget).only_integer }
 end
