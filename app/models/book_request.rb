b98e254220469c454d3d02f293d702a84326ee69
banguyen <nguyen.huu.ba@vareal.vn>
2019-07-25 14:17:23 +0700

    template-test-model-create-post-book-request

--- app/models/book_request.rb
+++ app/models/book_request.rb
@@ -2,5 +2,7 @@ class BookRequest < ApplicationRecord
   belongs_to :user
   has_many :book_request_image, dependent: :destroy
   validates :name, presence: true
-  validates :quantity, presence: true, numericality: { other_than: 0 }
+  validates :quantity, presence: true
+  validates :budget, presence: true
+
 end
