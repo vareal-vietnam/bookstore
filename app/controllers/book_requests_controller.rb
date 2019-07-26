5f54882ee394cacf80b00bcad6f4d2127c8efb43
banguyen <nguyen.huu.ba@vareal.vn>
2019-07-26 13:17:12 +0700

    test-1

--- app/controllers/book_requests_controller.rb
+++ app/controllers/book_requests_controller.rb
@@ -4,5 +4,19 @@ class BookRequestsController < ApplicationController
   end
 
   def create
+    binding.pry
+    if current_user
+      @book_request = current_user.book_requests.build(book_request_params)
+      binding.pry
+      if @book_request.save
+        redirect_to root_url
+      end
+    else
+      redirect_to new_session_url
+    end
   end
+  private
+    def book_request_params
+      params.require(:book_request).permit(:name, :comment, :budget, :quantity, :user_id)
+    end
 end
