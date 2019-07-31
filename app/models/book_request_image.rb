a9e7c7b7d6257a0e3e0c4eb175b739bf1ea88738
banguyen <nguyen.huu.ba@vareal.vn>
2019-07-31 11:20:34 +0700

    template

--- app/models/book_request_image.rb
+++ app/models/book_request_image.rb
@@ -2,4 +2,12 @@ class BookRequestImage < ApplicationRecord
   belongs_to :book_request
   validates :file, presence: true
   mount_uploader :file, RequestImageUploader
+
+  def url
+    file.url
+  end
+
+  def thumb_url
+    file.thumb_url
+  end
 end
