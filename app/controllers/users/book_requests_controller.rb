9c09ad2d1e972f4a245c903ee39be83b0c705b27
banguyen <nguyen.huu.ba@vareal.vn>
2019-08-06 10:27:32 +0700

    fix comment

--- app/controllers/users/book_requests_controller.rb
+++ app/controllers/users/book_requests_controller.rb
@@ -1,14 +1,18 @@
 module Users
   class BookRequestsController < ApplicationController
     def index
-      @book_requests = User.find(params[:user_id])
-                           .book_requests.order(created_at: :desc)
-                           .includes(:book_request_images, :user)
-                           .page(params[:page]).per(16)
+      @book_requests = book_requests_user
     end
 
     def edit
-      @book_request = BookRequest.find(params[:id])
+      @book_request = BookRequest.find_by(params[:id])
+    end
+
+    def book_requests_user
+      User.find(params[:user_id])
+          .book_requests.order(created_at: :desc)
+          .includes(:book_request_images, :user)
+          .page(params[:page]).per(16)
     end
   end
 end
