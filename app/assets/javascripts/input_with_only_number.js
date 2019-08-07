1c230380e155e3ccc5687e30a3a52dc5cfc03713
dinhnguyencanh <51991921+dinhnguyencanh@users.noreply.github.com>
2019-08-07 09:42:42 +0700

    Update input_with_only_number.js

--- app/assets/javascripts/input_with_only_number.js
+++ app/assets/javascripts/input_with_only_number.js
@@ -1,6 +1,6 @@
 $(function() {
     $(".number-field").keypress(function(event) {
-        if (event.which != 8 && event.which != 0 && (event.which < 48 || event.which > 57)) {
+        if (event.which < 48 || event.which > 57) {
             return false;
         }
     });
