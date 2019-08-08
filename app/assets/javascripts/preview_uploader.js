609be2398c50304e82b3c2d376610365f1b12ac0
Canh Dinh <nguyen.canh.dinh@vareal.vn>
2019-08-08 16:15:44 +0700

    Modify in preview_uploader.js

--- app/assets/javascripts/preview_uploader.js
+++ app/assets/javascripts/preview_uploader.js
@@ -1,13 +1,13 @@
 $(function() {
   function readURL(input) {
-    if (input.files && input.files[0]) {
+    if (input.files) {
       var reader = new FileReader();
 
       reader.onload = function (e) {
         $('#img_prev').attr('src', e.target.result);
       }
-      var i;
-      for (i = 0; i < input.files.length; i++) {
+
+      for (var i = 0; i < input.files.length; i++) {
         if(input.files[i].type.includes("image")) {
           reader.readAsDataURL(input.files[i]);
           break;
