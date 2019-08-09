8a9bcb8b813f0b634789aa4c476dc8a3d5c291d9
banguyenvareal <52641229+banguyenvareal@users.noreply.github.com>
2019-08-09 15:44:22 +0700

    logic and rspec for update book request (#79)

---
+++ spec/controllers/users/book_request/update_spec.rb
@@ -0,0 +1,89 @@
+require 'rails_helper'
+RSpec.describe Users::BookRequestsController, type: :controller do
+  include_context 'generate book_request_params'
+
+  let(:book_request) { create(:book_request, user_id: current_user.id) }
+
+  describe '#update' do
+    include_context 'logged in'
+    context 'valid invalid params' do
+      subject do
+        put :update, params: {
+          user_id: current_user.id, id: book_request.id,
+          book_request: invalid_book_request_params
+        }
+      end
+      it 'book request not change' do
+        expect { subject }.to_not change(book_request, :reload)
+      end
+
+      it 'should be render to edit' do
+        expect(subject).to render_template(:edit)
+      end
+    end
+
+    context 'with valid params with images' do
+      subject do
+        put :update, params: {
+          user_id: current_user.id, id: book_request.id,
+          book_request: valid_book_request_params
+        }
+      end
+
+      it 'book request have newest data' do
+        subject
+        expect(assigns(:book_request).attributes)
+          .to include(book_request_params.stringify_keys)
+      end
+
+      it 'the book is given enough image file' do
+        subject
+        expect(assigns(:book_request)
+          .book_request_images.count).to eql(image_files.count)
+      end
+
+      it 'return flash update success' do
+        subject
+        expect(flash.count).to eq(1)
+        expect(flash[:success]).to eql(I18n.t('book_requests.update.success'))
+      end
+
+      it 'redirect to view detail' do
+        subject
+        expect(subject)
+          .to redirect_to(user_book_request_url(current_user, book_request))
+      end
+    end
+
+    context 'with valid params and no image' do
+      subject do
+        put :update, params: {
+          user_id: current_user.id, id: book_request.id,
+          book_request: valid_book_request_params_no_image
+        }
+      end
+
+      it 'book request have newest data' do
+        subject
+        expect(assigns(:book_request)
+          .attributes).to include(book_request_params.stringify_keys)
+      end
+
+      it 'the book request images no change' do
+        expect { subject }.to_not change(book_request, :book_request_images)
+      end
+
+      it 'return flash update success' do
+        subject
+        expect(flash.count).to eq(1)
+        expect(flash[:success]).to eql(I18n.t('book_requests.update.success'))
+      end
+
+      it 'redirect to view detail' do
+        subject
+        expect(subject)
+          .to redirect_to(user_book_request_url(current_user, book_request))
+      end
+    end
+  end
+end
