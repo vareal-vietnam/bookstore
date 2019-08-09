268aabbffea657d6430f9f228368d3bc9fa46fc9
Canh Dinh <nguyen.canh.dinh@vareal.vn>
2019-08-09 10:08:28 +0700

    Fix bug 25

--- app/assets/javascripts/preview_uploader.js
+++ app/assets/javascripts/preview_uploader.js
@@ -1,22 +1,31 @@
 $(function() {
+  var defaultURL = $('#img_prev').attr('src');
   function showImagePreview(input) {
     if (input.files) {
       var reader = new FileReader();
-
+      var i, filesCount = input.files.length;
       reader.onload = function (e) {
         $('#img_prev').attr('src', e.target.result);
       }
-
-      for (var i = 0; i < input.files.length; i++) {
-        if(input.files[i].type.includes("image")) {
-          reader.readAsDataURL(input.files[i]);
+      for (i = 0; i < filesCount; i++) {
+        if(!(input.files[i].type.includes("image"))) {
+          alert("Input files must be image type!" );
+          $('#image-upload').wrap('<form>').closest('form').get(0).reset();
+          $('#image-upload').unwrap();
           break;
         }
+      }
+      if (i == filesCount) {
+        reader.readAsDataURL(input.files[0]);
       }
     }
   }
 
   $("#image-upload").change(function(){
     showImagePreview(this);
+  });
+
+  $("#image-upload").click(function(){
+    $('#img_prev').attr('src', defaultURL);
   });
 });
