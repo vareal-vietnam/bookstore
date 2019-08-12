a9a70b85aa2a85c36486c2d96471a0579f30d3d7
dinhnguyencanh <51991921+dinhnguyencanh@users.noreply.github.com>
2019-08-12 16:39:30 +0700

    fix bug 26 (#100)

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,6 +2,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
   process resize_to_fill: [600, 900]
 
+  def size_range
+    1..3.megabytes
+  end
+
   def extension_whitelist
     %w[jpg jpeg gif png]
   end
--- app/uploaders/request_image_uploader.rb
+++ app/uploaders/request_image_uploader.rb
@@ -2,6 +2,10 @@ class RequestImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
   process resize_to_fill: [600, 900]
 
+  def size_range
+    1..3.megabytes
+  end
+
   def extension_whitelist
     %w[jpg jpeg gif png]
   end
