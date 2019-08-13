2bc6bad112cdd4cf030602ad60888b91574cf4ad
Hieu Nguyen <10509903+hieunguyentrung@users.noreply.github.com>
2019-08-13 08:58:39 +0700

    fix thumb url

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,6 +1,11 @@
 module BooksHelper
   def get_thumb_url(book)
     image = book.images.first
+    if image
+      image.thumb_url
+    else
+      image_path('default-book-cover.jpg')
+    end
     image.thumb_url
   end
 end
